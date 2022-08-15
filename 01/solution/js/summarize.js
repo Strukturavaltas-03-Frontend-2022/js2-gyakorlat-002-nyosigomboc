7b50abd688a86b99190ce2b8a425523906430dc7
Gyozo Miklos <nyosdroid@gmail.com>
2022-08-15 04:08:45 +0200

    done

--- 01/solution/js/summarize.js
+++ 01/solution/js/summarize.js
@@ -1,7 +1,8 @@
+const goodNumber = num => Number.isInteger(num) || typeof num == 'bigint'
+const bigNumber = num => typeof num == 'bigint' || !Number.isSafeInteger(num)
 
+const summarize = (...numbers) => 
+    numbers.reduce( (prev, curr) => 
+        !goodNumber(curr) ? prev : ((bigNumber(prev)||bigNumber(curr)) ? BigInt(prev) + BigInt(curr) : prev + curr) )
 
-
-
-
-
-// export default summarize;
+export default summarize;
---
+++ 02/solution/js/numericConverter.js
@@ -0,0 +1,3 @@
+const numericConverter = num => ({binary: num.toString(2), octal: num.toString(8), hexa: num.toString(16)})
+
+export default numericConverter
